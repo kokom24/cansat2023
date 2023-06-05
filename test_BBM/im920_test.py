cdf7a0b20e808667dcd556ef399fa571649c0434
Arashi1023 <nagasaki@kimura-lab.net>
2023-06-05 18:17:41 +0900

    Update im920_test.py

--- test_BBM/im920_test.py
+++ test_BBM/im920_test.py
@@ -1,5 +1,4 @@
 import serial
-import random
 import time
 
 # シリアルポートの設定
@@ -9,17 +8,19 @@ baud_rate = 19200  # ボーレート
 # シリアルポートの初期化
 ser = serial.Serial(serial_port, baud_rate, timeout=3)
 
-while True:
-    # ランダムな数字の生成 (0から99まで)
-    random_number = random.randint(0, 99)
+def send_message(message):
+    # メッセージの送信
+    cmd = f'SENDB="{message}"\r\n'
+    ser.write(cmd.encode('utf-8'))
+    response = ser.readline().decode('utf-8').strip()
+    if response == 'OK':
+        print('Message sent successfully.')
+    else:
+        print(f'Failed to send message. Response: {response}')
 
-    # 数字の送信
-    data = f'{random_number}\r\n'.encode('utf-8')
-    ser.write(data)
-    print(f'Sent: {random_number}')
-
-    # 1秒待機
-    time.sleep(1)
+# メッセージの送信例
+message = 'Hello, IM920!'
+send_message(message)
 
 # シリアルポートのクローズ
 ser.close()
