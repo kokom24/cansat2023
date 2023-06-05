5110288c61cef9e0e102ad2933c16e889b79f458
Arashi1023 <nagasaki@kimura-lab.net>
2023-06-05 17:08:36 +0900

    Update im920_test.py

--- test_BBM/im920_test.py
+++ test_BBM/im920_test.py
@@ -1,11 +1,25 @@
 import serial
+import random
+import time
 
 # シリアルポートの設定
-ser = serial.Serial('/dev/serial0', 19200, timeout=1)
+serial_port = '/dev/ttyAMA0'  # 使用するシリアルポートのパス
+baud_rate = 19200  # ボーレート
 
-# メッセージの送信
-message = "Hello, IM920!"
-ser.write(message.encode())
+# シリアルポートの初期化
+ser = serial.Serial(serial_port, baud_rate, timeout=3)
+
+while True:
+    # ランダムな数字の生成 (0から99まで)
+    random_number = random.randint(0, 99)
+
+    # 数字の送信
+    data = f'{random_number}\r\n'.encode('utf-8')
+    ser.write(data)
+    print(f'Sent: {random_number}')
+
+    # 1秒待機
+    time.sleep(1)
 
 # シリアルポートのクローズ
 ser.close()
