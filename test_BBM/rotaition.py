d4ebb6c285c48664481bc8d9a347ecf51d734de8
Rion-Suzuki <129526724+Rion-Suzuki@users.noreply.github.com>
2023-06-12 14:09:06 +0900

    Update rotaition.py

--- test_BBM/rotaition.py
+++ test_BBM/rotaition.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 # モーターのピン番号を設定します
-left_motor_pin = 1  # 左モーターのピン番号正しいのに変える！
+left_motor_pin = 1  # 左モーターのピン番号
 right_motor_pin = 2  # 右モーターのピン番号
 
 # GPIOの設定
@@ -20,6 +20,14 @@ def rotate(duration):
 
 # メインの処理
 try:
-    rotate(1.2)  # 1.2秒間回転させることで約60°回転します、地面との関係によっても秒が変わるはず
+    gear_ratio = 30  # ギヤ比
+    input_rpm = 7000  # 入力軸の回転速度（RPM）
+
+    output_rpm = input_rpm / gear_ratio  # 出力軸の回転速度を計算します
+    rotation_per_sec = output_rpm / 60  # 1秒あたりの回転数を計算します
+
+    rotation_time = 60 / (1 * rotation_per_sec)  # 60°回転するために必要な時間を計算します
+
+    rotate(rotation_time)  # 計算された時間だけ回転させます
 except KeyboardInterrupt:
     GPIO.cleanup()
